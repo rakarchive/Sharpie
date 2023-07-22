8b43e187803aeae1b3885966b9d8322bc400fb52
SzilBalazs <szilprogramming@gmail.com>
2023-07-22 15:40:33 +0200

    PST

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,10 +1,25 @@
 ﻿using System;
-using System.Numerics;
 using ChessChallenge.API;
-using ChessChallenge.Application.APIHelpers;
 
 public class MyBot : IChessBot
 {
+    
+    public static readonly int[] pst =
+    {
+        100,  100,  100,  100,  105,  110,  110,   80,  100,  100,  100,  100,  100,  100,  100,  120, 
+        100,  100,  100,  125,  120,  120,  125,  130,  150,  150,  150,  150,  100,  100,  100,  100, 
+        250,  260,  270,  280,  260,  280,  290,  305,  270,  310,  315,  305,  280,  310,  310,  320, 
+        280,  310,  310,  320,  270,  310,  310,  305,  260,  280,  290,  305,  250,  260,  270,  280, 
+        280,  280,  280,  280,  300,  315,  310,  310,  300,  320,  320,  320,  300,  310,  320,  320, 
+        300,  310,  320,  320,  300,  310,  320,  320,  300,  310,  310,  310,  290,  310,  310,  310, 
+        480,  500,  505,  510,  485,  500,  500,  500,  490,  500,  500,  500,  490,  500,  500,  500, 
+        490,  500,  500,  500,  490,  500,  500,  500,  520,  520,  520,  520,  500,  500,  500,  500, 
+        900,  900,  900,  900,  900,  900,  900,  900,  900,  900,  900,  900,  900,  900,  900,  900, 
+        900,  900,  900,  900,  900,  900,  900,  900,  900,  900,  900,  900,  900,  900,  900,  900, 
+         20,   30,  -30,    0,   10,   10,  -40,  -40,  -40,  -40,  -40,  -40,  -40,  -40,  -40,  -40, 
+        -40,  -40,  -40,  -40,  -40,  -40,  -40,  -40,  -40,  -40,  -40,  -40,  -40,  -40,  -40,  -40,
+    };
+    
     public Move Think(Board board, Timer timer)
     {
         const int INF = int.MaxValue - 1;
@@ -51,24 +66,29 @@ public class MyBot : IChessBot
             
             return bestEvaluation;
         }
+        
+        int GetPstIndex(bool pc, int pt, int square)
+        {
+            if (pc == false) square ^= 56;
+            if ((square & 4) != 0) square ^= 7;
+            return (pt - 1) * 32 + (square & 3 | square >> 3 << 2);
+        }
 
         int Evaluate()
         {
-            bool isWhite = board.IsWhiteToMove;
-            bool isBlack = !isWhite           ;
-            return Difference(PieceType.Pawn  ) +
-                   Difference(PieceType.Knight) * 3 +
-                   Difference(PieceType.Bishop) * 3 +
-                   Difference(PieceType.Rook  ) * 5 +
-                   Difference(PieceType.Queen ) * 9 ;
-
-            int Difference(PieceType pieceType)
+            int result = 0;
+            for (int sq = 0; sq < 64; sq++)
             {
-                return BitOperations.PopCount(board.GetPieceBitboard(pieceType, isWhite)) -
-                       BitOperations.PopCount(board.GetPieceBitboard(pieceType, isBlack));
+                Piece piece = board.GetPiece(new Square(sq));
+                if (piece.PieceType == PieceType.None) continue;
+                int index = GetPstIndex(piece.IsWhite, (int)piece.PieceType, sq);
+                if (piece.IsWhite == board.IsWhiteToMove) result += pst[index];
+                else result -= pst[index];
             }
+
+            return result;
         }
-        
+
         return bestMove;
     }
     
